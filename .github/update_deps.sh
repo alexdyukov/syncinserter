0aae9a755d79e4d537418a44004c62da4b052217
Alex Dyukov <alex.dyukov.93@gmail.com>
2026-01-03 15:29:54 +0300

    FIX: update ci

--- .github/update_deps.sh
+++ .github/update_deps.sh
@@ -13,11 +13,14 @@ git remote set-url origin https://x-access-token:${GH_TOKEN}@github.com/${REPO_N
 git checkout -b ${BRANCH_NAME}
 
 # 4. commit changes or fast exit
-git commit -am "Fix: $(date +%F) update dependencies" || exit 0
+git commit -am "FIX: $(date +%F) update dependencies" || exit 0
 git push --set-upstream origin ${BRANCH_NAME} -f || exit 0
 
 # 5. run tests after update
 go test -bench=. -benchmem -benchtime=100000x > new.txt
 
 # 6. create PR with benchmark difference
-gh pr create -a ${REPO_OWNER} -b "$(benchstat old.txt new.txt)" --fill
+echo '```' > github_body.txt
+benchstat old.txt new.txt >> github_body.txt
+echo '```' >> github_body.txt
+gh pr create -a ${REPO_OWNER} -F github_body.txt --fill
