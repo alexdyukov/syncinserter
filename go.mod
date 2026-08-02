cd2072f9aa5055970594fac18527cb2f6900a091
Alex Dyukov <alex.dyukov.93@gmail.com>
2026-08-02 21:43:01 +0300

    Refactor for parametrized row

--- go.mod
+++ go.mod
@@ -1,8 +1,8 @@
-module github.com/alexdyukov/syncinserter
+module github.com/alexdyukov/syncinserter/v2
 
-go 1.25.0
+go 1.25.9
 
-replace github.com/alexdyukov/syncinserter => ./
+replace github.com/alexdyukov/syncinserter/v2 => ./
 
 require (
 	github.com/ClickHouse/clickhouse-go/v2 v2.47.0
@@ -46,7 +46,7 @@ require (
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/klauspost/compress v1.19.1 // indirect
-	github.com/lufia/plan9stats v0.0.0-20260627054121-477a66015f15 // indirect
+	github.com/lufia/plan9stats v0.0.0-20260802145828-341c2f0c90b5 // indirect
 	github.com/magiconair/properties v1.18.11 // indirect
 	github.com/mdelapenya/tlscert v0.2.0 // indirect
 	github.com/moby/docker-image-spec v1.3.1 // indirect
--- syncinserter_test.go
+++ syncinserter_test.go
@@ -3,6 +3,7 @@ package syncinserter_test
 import (
 	"context"
 	"errors"
+	"fmt"
 	"math"
 	"math/rand"
 	"os"
@@ -11,13 +12,14 @@ import (
 	"testing"
 	"time"
 
-	clickhouse "github.com/ClickHouse/clickhouse-go/v2"
-	"github.com/alexdyukov/syncinserter"
+	"github.com/ClickHouse/clickhouse-go/v2"
+	"github.com/alexdyukov/syncinserter/v2"
 	"github.com/gocql/gocql"
 	"github.com/google/uuid"
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/testcontainers/testcontainers-go"
+
 	cassandraTestContainer "github.com/testcontainers/testcontainers-go/modules/cassandra"
 	clickhouseTestContainer "github.com/testcontainers/testcontainers-go/modules/clickhouse"
 	postgresTestContainer "github.com/testcontainers/testcontainers-go/modules/postgres"
@@ -25,20 +27,21 @@ import (
 	"go.uber.org/goleak"
 )
 
-const (
-	// for example visa processing 24,000 Transactions per Second and we do benchParallelism*GOMAXPROCS.
-	benchParallelism = 10000
-)
+type row struct {
+	createdAt time.Time
+	usr       uuid.UUID
+	diff      float64
+}
 
-var uuids []string
+var uuids []uuid.UUID
 
 func TestMain(m *testing.M) {
 	const uuidCount = 5000
 
-	uuids = make([]string, 0, uuidCount)
+	uuids = make([]uuid.UUID, 0, uuidCount)
 
 	for range uuidCount {
-		uuids = append(uuids, uuid.NewString())
+		uuids = append(uuids, uuid.New())
 	}
 
 	os.Exit(m.Run())
@@ -171,7 +174,7 @@ func BenchmarkOverhead(b *testing.B) {
 	ctx, cancel := context.WithCancel(b.Context())
 	defer cancel()
 
-	insertFunc := func(rows [][]any) error { return nil }
+	insertFunc := func(rows []row) error { return nil }
 
 	const batchSize = 1000
 
@@ -180,17 +183,14 @@ func BenchmarkOverhead(b *testing.B) {
 		b.Fatal(err.Error())
 	}
 
-	b.SetParallelism(benchParallelism)
 	b.StartTimer()
 
-	b.RunParallel(func(p *testing.PB) {
-		for p.Next() {
-			err = inserter.Insert(ctx, []any{})
-			if err != nil {
-				b.Fatal(err.Error())
-			}
+	for b.Loop() {
+		err = inserter.Insert(ctx, row{})
+		if err != nil {
+			b.Fatal(err.Error())
 		}
-	})
+	}
 }
 
 func BenchmarkPostgres(b *testing.B) {
@@ -198,13 +198,10 @@ func BenchmarkPostgres(b *testing.B) {
 	// defer goleak.VerifyNone(b)
 	b.StopTimer()
 
-	ctx, cancel := context.WithCancel(b.Context())
-	defer cancel()
-
 	testName := strings.ToLower(b.Name())
 
 	container, err := postgresTestContainer.Run(
-		ctx,
+		b.Context(),
 		"postgres:latest",
 		testcontainers.WithName(testName),
 		testcontainers.WithWaitStrategy(wait.ForListeningPort("5432/tcp")),
@@ -213,9 +210,9 @@ func BenchmarkPostgres(b *testing.B) {
 		b.Fatal(err.Error())
 	}
 
-	defer container.Terminate(ctx)
+	defer container.Terminate(context.Background())
 
-	connString, err := container.ConnectionString(ctx)
+	connString, err := container.ConnectionString(b.Context())
 	if err != nil {
 		b.Fatal(err.Error())
 	}
@@ -225,47 +222,75 @@ func BenchmarkPostgres(b *testing.B) {
 		b.Fatal(err.Error())
 	}
 
-	conn, err := pgxpool.NewWithConfig(ctx, config)
+	conn, err := pgxpool.NewWithConfig(b.Context(), config)
 	if err != nil {
 		b.Fatal(err.Error())
 	}
 
 	defer conn.Close()
 
-	err = conn.Ping(ctx)
+	err = conn.Ping(b.Context())
 	if err != nil {
 		b.Fatal(err.Error())
 	}
 
-	_, err = conn.Exec(ctx, `CREATE TABLE IF NOT EXISTS `+testName+` (created_at TIMESTAMP, usr UUID, diff double precision);`)
+	_, err = conn.Exec(b.Context(), `CREATE TABLE IF NOT EXISTS `+testName+` (created_at TIMESTAMP, usr UUID, diff double precision);`)
 	if err != nil {
 		b.Fatal(err.Error())
 	}
 
 	insertFunc := func(rows [][]any) error {
-		_, err := conn.CopyFrom(ctx, pgx.Identifier{testName}, []string{"created_at", "usr", "diff"}, pgx.CopyFromRows(rows))
+		_, err := conn.CopyFrom(b.Context(), pgx.Identifier{testName}, []string{"created_at", "usr", "diff"}, pgx.CopyFromRows(rows))
 
 		return err
 	}
 
-	const batchSize = 1000
+	batchSizes := []int{100, 1000, 10000}
+	for _, batchSize := range batchSizes {
+		for parallelism := batchSize / 10; parallelism <= batchSize*10 && parallelism <= batchSizes[len(batchSizes)-1]; parallelism *= 10 {
+			b.Run(fmt.Sprintf("BatchSize%dParallelism%d", batchSize, parallelism), func(b *testing.B) {
+				ctx, cancel := context.WithCancel(b.Context())
+				defer cancel()
 
-	inserter, err := syncinserter.New(ctx, insertFunc, batchSize, time.Duration(1))
-	if err != nil {
-		b.Fatal(err.Error())
-	}
+				inserter, err := syncinserter.New(ctx, insertFunc, batchSize, time.Duration(1))
+				if err != nil {
+					b.Fatal(err.Error())
+				}
 
-	b.SetParallelism(benchParallelism)
-	b.StartTimer()
+				readyCh := make(chan struct{})
+				closeCh := make(chan struct{})
 
-	b.RunParallel(func(p *testing.PB) {
-		for p.Next() {
-			err = inserter.Insert(ctx, []any{time.Now().UTC(), uuids[rand.Intn(len(uuids))], rand.Float64()})
-			if err != nil {
-				b.Fatal(err.Error())
-			}
+				for range parallelism - 1 {
+					go func() {
+						<-readyCh
+
+						for {
+							select {
+							case <-closeCh:
+								return
+							default:
+								_ = inserter.Insert(ctx, []any{time.Now().UTC(), uuids[rand.Intn(len(uuids))], rand.Float64()})
+							}
+						}
+					}()
+				}
+
+				b.StartTimer()
+
+				close(readyCh)
+				defer close(closeCh)
+
+				for b.Loop() {
+					err = inserter.Insert(ctx, []any{time.Now().UTC(), uuids[rand.Intn(len(uuids))], rand.Float64()})
+					if err != nil {
+						b.Fatal(err.Error())
+					}
+				}
+
+				b.StopTimer()
+			})
 		}
-	})
+	}
 }
 
 func BenchmarkClickhouse(b *testing.B) {
@@ -273,13 +298,10 @@ func BenchmarkClickhouse(b *testing.B) {
 	// defer goleak.VerifyNone(b)
 	b.StopTimer()
 
-	ctx, cancel := context.WithCancel(b.Context())
-	defer cancel()
-
 	testName := strings.ToLower(b.Name())
 
 	container, err := clickhouseTestContainer.Run(
-		ctx,
+		b.Context(),
 		"clickhouse/clickhouse-server:latest",
 		testcontainers.WithName(testName),
 		clickhouseTestContainer.WithUsername(testName),
@@ -289,9 +311,9 @@ func BenchmarkClickhouse(b *testing.B) {
 		b.Fatal(err.Error())
 	}
 
-	defer container.Terminate(ctx)
+	defer container.Terminate(context.Background())
 
-	connString, err := container.ConnectionString(ctx)
+	connString, err := container.ConnectionString(b.Context())
 	if err != nil {
 		b.Fatal(err.Error())
 	}
@@ -308,25 +330,25 @@ func BenchmarkClickhouse(b *testing.B) {
 
 	defer conn.Close()
 
-	err = conn.Ping(ctx)
+	err = conn.Ping(b.Context())
 	if err != nil {
 		b.Fatal(err.Error())
 	}
 
-	err = conn.Exec(ctx, `CREATE TABLE IF NOT EXISTS `+testName+` (created_at DateTime64(9, 'UTC'), usr UUID, diff Float64) ENGINE = MergeTree() ORDER BY created_at;`)
+	err = conn.Exec(b.Context(), `CREATE TABLE IF NOT EXISTS `+testName+` (created_at DateTime64(9, 'UTC'), usr UUID, diff Float64) ENGINE = MergeTree() ORDER BY created_at;`)
 	if err != nil {
 		b.Fatal(err.Error())
 	}
 
-	insertFunc := func(rows [][]any) error {
-		batch, err := conn.PrepareBatch(ctx, `INSERT INTO `+testName+` (created_at, usr, diff)`)
+	insertFunc := func(rows []row) error {
+		batch, err := conn.PrepareBatch(b.Context(), `INSERT INTO `+testName+` (created_at, usr, diff)`)
 		if err != nil {
 			return err
 		}
 		defer batch.Close()
 
 		for _, row := range rows {
-			err = batch.Append(row...)
+			err = batch.Append(row.createdAt, row.usr, row.diff)
 			if err != nil {
 				return err
 			}
@@ -338,24 +360,64 @@ func BenchmarkClickhouse(b *testing.B) {
 	// https://clickhouse.com/docs/optimize/bulk-inserts
 	// We recommend inserting data in batches of at least 1,000 rows, and ideally between 10,000–100,000 rows.
 	// Fewer, larger inserts reduce the number of parts written, minimize merge load, and lower overall system resource usage.
-	const batchSize = 10000
+	batchSizes := []int{1000, 10000, 100000}
+	for _, batchSize := range batchSizes {
+		for parallelism := batchSize / 10; parallelism <= batchSize*10 && parallelism <= batchSizes[len(batchSizes)-1]; parallelism *= 10 {
+			b.Run(fmt.Sprintf("BatchSize%dParallelism%d", batchSize, parallelism), func(b *testing.B) {
+				ctx, cancel := context.WithCancel(b.Context())
+				defer cancel()
 
-	inserter, err := syncinserter.New(ctx, insertFunc, batchSize, time.Duration(1))
-	if err != nil {
-		b.Fatal(err.Error())
-	}
+				inserter, err := syncinserter.New(ctx, insertFunc, batchSize, time.Duration(1))
+				if err != nil {
+					b.Fatal(err.Error())
+				}
 
-	b.SetParallelism(benchParallelism)
-	b.StartTimer()
+				readyCh := make(chan struct{})
+				closeCh := make(chan struct{})
 
-	b.RunParallel(func(p *testing.PB) {
-		for p.Next() {
-			err = inserter.Insert(ctx, []any{time.Now(), uuids[rand.Intn(len(uuids))], rand.Float64()})
-			if err != nil {
-				b.Fatal(err.Error())
-			}
+				for range parallelism - 1 {
+					go func() {
+						<-readyCh
+
+						for {
+							select {
+							case <-closeCh:
+								return
+							default:
+								r := row{
+									createdAt: time.Now().UTC(),
+									usr:       uuids[rand.Intn(len(uuids))],
+									diff:      rand.Float64(),
+								}
+
+								_ = inserter.Insert(ctx, r)
+							}
+						}
+					}()
+				}
+
+				b.StartTimer()
+
+				close(readyCh)
+				defer close(closeCh)
+
+				for b.Loop() {
+					r := row{
+						createdAt: time.Now().UTC(),
+						usr:       uuids[rand.Intn(len(uuids))],
+						diff:      rand.Float64(),
+					}
+
+					err = inserter.Insert(ctx, r)
+					if err != nil {
+						b.Fatal(err.Error())
+					}
+				}
+
+				b.StopTimer()
+			})
 		}
-	})
+	}
 }
 
 func BenchmarkCassandra(b *testing.B) {
@@ -395,42 +457,79 @@ func BenchmarkCassandra(b *testing.B) {
 
 	defer session.Close()
 
-	session.Closed()
-
 	err = session.Query(`CREATE TABLE IF NOT EXISTS ` + testName + ` (created_at TIMESTAMP, usr UUID, diff DOUBLE, PRIMARY KEY (created_at, usr));`).Exec()
 	if err != nil {
 		b.Fatal(err.Error())
 	}
 
-	insertFunc := func(rows [][]any) error {
+	insertFunc := func(rows []row) error {
 		batch := session.NewBatch(gocql.LoggedBatch)
 
 		insertStr := `INSERT INTO ` + testName + ` (created_at, usr, diff) VALUES (?, ?, ?)`
 
 		for _, row := range rows {
-			batch.Query(insertStr, row...)
+			batch.Query(insertStr, row.createdAt, row.usr.String(), row.diff)
 		}
 
 		return session.ExecuteBatch(batch)
 	}
 
-	// Batch too large for 1000+
-	const batchSize = 500
+	batchSizes := []int{100, 250, 500}
+	for _, batchSize := range batchSizes {
+		for parallelism := batchSize / 10; parallelism <= batchSize*10 && parallelism <= batchSizes[len(batchSizes)-1]; parallelism *= 10 {
+			b.Run(fmt.Sprintf("BatchSize%dParallelism%d", batchSize, parallelism), func(b *testing.B) {
+				ctx, cancel := context.WithCancel(b.Context())
+				defer cancel()
 
-	inserter, err := syncinserter.New(ctx, insertFunc, batchSize, time.Duration(1))
-	if err != nil {
-		b.Fatal(err.Error())
-	}
+				inserter, err := syncinserter.New(ctx, insertFunc, batchSize, time.Duration(1))
+				if err != nil {
+					b.Fatal(err.Error())
+				}
 
-	b.SetParallelism(benchParallelism)
-	b.StartTimer()
+				readyCh := make(chan struct{})
+				closeCh := make(chan struct{})
 
-	b.RunParallel(func(p *testing.PB) {
-		for p.Next() {
-			err = inserter.Insert(ctx, []any{time.Now(), uuids[rand.Intn(len(uuids))], rand.Float64()})
-			if err != nil {
-				b.Fatal(err.Error())
-			}
+				for range parallelism - 1 {
+					go func() {
+						<-readyCh
+
+						for {
+							select {
+							case <-closeCh:
+								return
+							default:
+								r := row{
+									createdAt: time.Now().UTC(),
+									usr:       uuids[rand.Intn(len(uuids))],
+									diff:      rand.Float64(),
+								}
+
+								_ = inserter.Insert(ctx, r)
+							}
+						}
+					}()
+				}
+
+				b.StartTimer()
+
+				close(readyCh)
+				defer close(closeCh)
+
+				for b.Loop() {
+					r := row{
+						createdAt: time.Now().UTC(),
+						usr:       uuids[rand.Intn(len(uuids))],
+						diff:      rand.Float64(),
+					}
+
+					err = inserter.Insert(ctx, r)
+					if err != nil {
+						b.Fatal(err.Error())
+					}
+				}
+
+				b.StopTimer()
+			})
 		}
-	})
+	}
 }
